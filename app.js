2b19ba9ce55e64e18a2d014bbf0891ce419da15b
William Gibson <wdr.gibson@gmail.com>
2020-05-20 19:01:17 -0600

    Created arrays populated with IDs of HTML elements for each person's name, birth, and homeworld. Updated updateInfo function to change text values of HTML elements in arrays to display data received from api. Api returns url of planet data, so required to make second HTTP request in order to acquire homeworld names. Also added functions to display error when HTTP requests fail

--- app.js
+++ app.js
@@ -1,15 +1,59 @@
-let button = document.querySelector('#searchButton');
-let name = document.querySelector('#name');
+//initialize data arrays and declare next button
+let nameArray   = [];
+let birthArray  = [];
+let worldArray  = [];
+let nextBtn     = document.querySelector('#nextBtn');
 
+//populate arrays with references to proper html elements
+for(let i=0; i<10; i++){
+
+  nameArray[i]  = document.querySelector('#name' + i).childNodes[0];
+  birthArray[i] = document.querySelector('#birth' + i);
+  worldArray[i] = document.querySelector('#world' + i);
+}
 
 function getInfo(){
-  axios.get('https://swapi.dev/api/people/1').then(function(response){
-    updateInfo(response.data);
-  });
+
+  let swapiURL = 'https://swapi.dev/api/people/';
+
+  //get data from server, update html elements with data
+  axios.get(swapiURL).then(function(response){
+    updateInfo(response.data.results);
+  }).catch(function(){
+    updateInfoError();
+  })
 }
 
 function updateInfo(data){
-  name.innerText = data.name;
+
+  for(let i=0; i< 10; i++){
+
+    nameArray[i].nodeValue  = data[i].name; //update nodeValue so nested lists aren't overwritten
+    birthArray[i].innerText = `Birth Year:  ${data[i].birth_year}`;
+
+    //data[i].homeworld returns url. Request and pass planet data to updatePlanetName(data)
+    axios.get(data[i].homeworld).then(function(response){
+      worldArray[i].innerText = "Home planet: " + response.data.name; 
+    }).catch;
+  }
 }
 
-button.addEventListener('click', getInfo);
+function updateInfoError(){
+
+  for(let i=0; i< 10; i++){
+
+    nameArray[i].innerText    = 'Oops, error!';
+    birthArray[i].innerText   = '';
+    worldArray[i].innerText   = '';
+  }
+}
+
+function updateWorldsError(){
+
+  for(let i=0; i< 10; i++){
+
+    worldArray[i].innerText = 'Oops, error!'
+  }
+}
+
+nextBtn.addEventListener('click', getInfo);
